93dd1499a1b6831752f49cfdc1285705e0a14ea5
Liya Du <liydu@microsoft.com>
2020-01-28 16:19:42 +0800

    The bare-metal app is able to run on KaiOS

--- kaido-app/src/components/app.tsx
+++ kaido-app/src/components/app.tsx
@@ -6,7 +6,6 @@ import Profile from "../routes/profile"
 import Header from "./header"
 
 if ((module as any).hot) {
-  // tslint:disable-next-line:no-var-requires
   /* eslint-disable global-require */
   require(`preact/debug`)
   /* eslint-enable global-require */
